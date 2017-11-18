002812e8f0fe02f4e12af5bc8c88670243f5b75d
Matt Larson <matt.a.larson@gmail.com>
2017-11-18 10:44:47 -0600

    basic setup of react-router-dom

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
   the next one will accept a POST type request to create a brand new blog post and save it to server
   the first one fetches a particular post based on ID
   last one can DELETE post based on ID
+
+## react-router-dom
+
+`import { BrowserRouter, Route, Link } from 'react-router-dom'`
+  BrowswerRouter: interacts with the history library and decides what to do based on url change. 
+  Route: react component that can be rendered inside any other component inside application. The purpose is to provide the configuration of which component to show based on url
+  Link: 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import ReduxPromise from 'redux-promise'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 import App from './App'
 import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+class Hello extends React.Component {
+  render() { 
+    return (
+      <div>
+        Hello!
+        <Link to='/goodbye'>Goodbye</Link>
+      </div> 
+    )
+  }
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div> }
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router>
+      <div>
+        <Route path='/hello' component={Hello} />
+        <Route path='/goodbye' component={Goodbye}/>
+      </div>
+    </Router>
   </Provider>
   , document.getElementById('root'))
