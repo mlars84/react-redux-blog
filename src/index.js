aa16745a713668fba7d73d6aa565de71900d8c22
Matt Larson <matt.a.larson@gmail.com>
2017-11-18 10:48:40 -0600

    refactored rrd for simple home component

--- src/index.js
+++ src/index.js
@@ -3,33 +3,18 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from './components/Home'
 
 import App from './App'
 import reducers from './reducers'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore)
 
-class Hello extends React.Component {
-  render() { 
-    return (
-      <div>
-        Hello!
-        <Link to='/goodbye'>Goodbye</Link>
-      </div> 
-    )
-  }
-}
-
-class Goodbye extends React.Component {
-  render() { return <div>Goodbye!</div> }
-}
-
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router>
       <div>
-        <Route path='/hello' component={Hello} />
-        <Route path='/goodbye' component={Goodbye}/>
+        <Route path='/' component={Home}/>
       </div>
     </Router>
   </Provider>
