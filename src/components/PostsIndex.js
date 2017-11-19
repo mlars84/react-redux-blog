3df70f57ac3058713577ab7fbce5a7c4c18be067
Matt Larson <matt.a.larson@gmail.com>
2017-11-19 10:10:03 -0600

    componentDidMount setup and API working, but no posts yet

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -3,11 +3,19 @@ import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
 class PostsIndex extends Component {
+  //lifecycle method that is invoked immediately after a component is mounted to the DOM.
+  //good place to load data from an API
+  componentDidMount() {
+    this.props.fetchPosts()
+  }
+
   render() {
     return (
-      <div>Posts Index</div>
+      <div>
+        Posts Index
+      </div>
     )
   }
 }
 
-export default PostsIndex
+export default connect(null, { fetchPosts })(PostsIndex)
