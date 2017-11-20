96002d5a43e30afba45fce4cfc1896547638a563
Matt Larson <matt.a.larson@gmail.com>
2017-11-20 10:14:11 -0600

    setup new route and basic component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import PostsIndex from './components/PostsIndex'
+import PostsNew from './components/PostsNew'
 import promise from 'redux-promise'
 
 import reducers from './reducers'
@@ -14,7 +15,8 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router>
       <div>
-        <Route exact path='/' component={PostsIndex } />
+        <Route exact path='/' component={ PostsIndex } />
+        <Route path='/new' component={ PostsNew } />
       </div>
     </Router>
   </Provider>
