8160b4981abaa49de1e9d72cce60e28e3346743b
Matt Larson <matt.a.larson@gmail.com>
2017-11-23 09:09:33 -0600

    starting reduxForm

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>
-        Posts New!
-      </div>
+      <form>
+        <Field 
+          name='title'
+          component={}
+        />
+      </form>
     )
   }
 }
 
-export default PostsNew
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew)
