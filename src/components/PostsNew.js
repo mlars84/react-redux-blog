e34b14b02e1a2408e6a98fe6c7dd4fbfc7e6aae8
Matt Larson <matt.a.larson@gmail.com>
2017-11-23 09:26:59 -0600

    rendering inputs

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -2,12 +2,36 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
+  renderTitleField(field) {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Title"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
   render() {
     return (
       <form>
-        <Field 
-          name='title'
-          component={}
+        <label>First Name</label>
+        <Field
+          name="title"
+          component={this.renderTitleField}
+        />
+        <Field
+          name="firstName"
+          component="input"
+          type="text"
+          placeholder="First Name"
+        />
+        <Field
+          name="content"
+          component="textarea"
+          type="textarea"
         />
       </form>
     )
