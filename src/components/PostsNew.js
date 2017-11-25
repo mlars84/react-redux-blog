d671cbc45657d24306f6a80f599e980c2768d97c
Matt Larson <matt.a.larson@gmail.com>
2017-11-25 10:01:22 -0600

    created validate function for validating form

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -24,8 +24,8 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <Field
-          name="tags"
-          label='Tags'
+          name="categories"
+          label='Categories '
           component={this.renderField}
         />
         <Field
@@ -38,6 +38,25 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  console.log(values)
+  const errors = {}
+
+  //validate the inputs from 'values'
+  if (!values.title) {
+    errors.title = 'Enter a title!'
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some Categories!'
+  }
+  if (!values.content) {
+    errors.content = 'Enter some Content!'
+  }
+
+  return errors
+}
+  
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew)
